162a536da43fba6dad045c29a8d1e4ef5d080e8a
ISwMF <31453580+ISwMF@users.noreply.github.com>
2020-07-31 18:03:18 -0500

    Update agregartienda.blade.php

--- TredaSolutions/resources/views/agregartienda.blade.php
+++ TredaSolutions/resources/views/agregartienda.blade.php
@@ -21,7 +21,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <link rel="stylesheet" href="{{ asset('css/left-panel.css')}}">
-    <title>Document</title>
+    <title>Agregar tienda</title>
 </head>
 
 <body>
